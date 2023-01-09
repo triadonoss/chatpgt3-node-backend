c1ff6e4efcc2afb1b749ac7f3e37bd5362212f04
triadonoss <85204936+triadonoss@users.noreply.github.com>
2023-01-09 23:51:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chatpgt3-node-backend
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-4qs9pk)
---
+++ server.js
@@ -0,0 +1,39 @@
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3500;
+const cors = require('cors');
+
+const configuration = new Configuration({
+  organization: 'org-Mlt1HHrtFYtlPXokEqrcVPAi',
+  apiKey: process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (true) {
+        callback(null, true);
+      }
+    },
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(express.json());
+app.post('/', async (req, res) => {
+  res.send('Yes it works!');
+  const { promps } = req.body;
+
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: promps,
+    max_tokens: 1000,
+    temperature: 0,
+  });
+
+  res.send(response.data?.choices);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
